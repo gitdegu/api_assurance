11e4f7a5cfab3b927de2bbb6909818ed0b52c38b
degu <adnewdilu@gmail.com>
2019-09-23 23:01:12 +0200

    Test Cases

---
+++ src/test/java/getRequest/GetData.java
@@ -0,0 +1,28 @@
+package getRequest;
+
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import junit.framework.Assert;
+
+public class GetData {
+	
+	
+	public void testResponseCode() {
+		
+		Response res=get("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+		int statucode=res.getStatusCode();
+		System.out.print("Stats code is=" + statucode);
+		Assert.assertEquals(statucode, 200);
+	}
+	@Test
+	public void testBody() {
+		Response res=get("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+		String data=res.asString();
+		System.out.println("Data is=" + data);
+		System.out.println("Response time=" + res.getTime());
+		
+	}
+}
